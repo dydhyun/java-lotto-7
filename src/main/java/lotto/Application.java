1f511a0df3ea7467a439f87c6ff3f2982f5d9ffb
dydhyun <dydgus625@naver.com>
2024-11-03 19:06:51 +0900

    feat: 천원 단위로 로또 구입금액 입력받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,8 +2,32 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
+
 //         TODO: 프로그램 구현
+
+        while (true) {
+            System.out.println("구입 금액을 입력하세요 : ");
+            long inputAmount = Long.parseLong(camp.nextstep.edu.missionutils.Console.readLine());
+
+            try {
+                long count = Lotto.validateInputAmount(inputAmount);
+                System.out.println(count + " 회 진행합니다.");
+                break;
+            } catch (IllegalArgumentException ie) {
+                System.out.println(ie.getMessage());
+            }
+        }
+
+        while (true) {
+            System.out.println("당첨 번호를 입력하세요 : ");
+            String targetNumberStr = camp.nextstep.edu.missionutils.Console.readLine();
+
+
+            System.out.println("보너스 번호를 입력하세요 : ");
+            String bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
+        }
 
 
     }
+
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -18,4 +18,12 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
 
+    // TODO: 천원단위로 투입했는지 검증하기
+    public static long validateInputAmount(long inputAmount) {
+        if(inputAmount % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 천원 단위로 해주세요.");
+        }
+        return inputAmount / 1000;
+    }
+
 }
