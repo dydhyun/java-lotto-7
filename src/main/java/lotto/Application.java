a4a47c97d71eb028825e36280be5e073a2676333
dydhyun <dydgus625@naver.com>
2024-11-04 12:05:03 +0900

    feat: 랜덤 로또번호 출력하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,30 +1,43 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
 //         TODO: 프로그램 구현
 
+        long purchaseCount = 0;
+        List<List<Integer>> generatedLottoNumbers = new ArrayList<>();
+
+        // TODO: 메서드로 분리하기
         while (true) {
             System.out.println("구입 금액을 입력하세요 : ");
-            long inputAmount = Long.parseLong(camp.nextstep.edu.missionutils.Console.readLine());
+            long inputAmount = Long.parseLong(Console.readLine());
 
             try {
-                long count = Lotto.validateInputAmount(inputAmount);
-                System.out.println(count + " 회 진행합니다.");
+                purchaseCount = Lotto.validateInputAmount(inputAmount);
+                System.out.println(purchaseCount + " 개를 구매했습니다.");
                 break;
             } catch (IllegalArgumentException ie) {
                 System.out.println(ie.getMessage());
             }
         }
 
+        generatedLottoNumbers = Lotto.generateLotto(purchaseCount);
+
+        Lotto.printLottoNumbers(generatedLottoNumbers);
+
         while (true) {
             System.out.println("당첨 번호를 입력하세요 : ");
-            String targetNumberStr = camp.nextstep.edu.missionutils.Console.readLine();
+            String targetNumberStr = Console.readLine();
 
 
             System.out.println("보너스 번호를 입력하세요 : ");
-            String bonusNumber = camp.nextstep.edu.missionutils.Console.readLine();
+            String bonusNumber = Console.readLine();
         }
 
 
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -20,10 +24,37 @@ public class Lotto {
 
     // TODO: 천원단위로 투입했는지 검증하기
     public static long validateInputAmount(long inputAmount) {
+        if(inputAmount < 1000){
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 천원 이상으로 해주세요.");
+        }
         if(inputAmount % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 투입 금액은 천원 단위로 해주세요.");
         }
         return inputAmount / 1000;
     }
+
+    // TODO: 천원단위로 투입했는지 검증하기
+    public static List<List<Integer>> generateLotto(long purchaseCount) {
+        List<List<Integer>> randomLottoList = new ArrayList<>();
+
+        if(purchaseCount < 0){
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 천원 이상으로 해주세요.");
+        }
+        for (int i = 0; i < purchaseCount; i++) {
+            List<Integer> singleLotto = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(singleLotto);
+            randomLottoList.add(singleLotto);
+        }
+
+        return randomLottoList;
+    }
+
+    // TODO: 생성된 로또 번호 출력하기
+    public static void printLottoNumbers(List<List<Integer>> generatedLottoNumbers) {
+        for (List<Integer> lotto : generatedLottoNumbers) {
+            System.out.println(lotto);
+        }
+    }
+
 
 }
